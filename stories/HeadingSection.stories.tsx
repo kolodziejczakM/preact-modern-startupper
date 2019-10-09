b3089a671abc6bf6fd3fc28bb9480a9d2e792c35
kolodziejczakM <kolodziejczak.mn@gmail.com>
2019-10-09 22:09:35 +0200

    Added storybook

---
+++ stories/HeadingSection.stories.tsx
@@ -0,0 +1,8 @@
+import { h } from 'preact';
+import { HeadingSection } from '../src/App/App.styles';
+
+export default {
+    title: 'HeadingSection',
+};
+
+export const withSampleText = (): any => <HeadingSection>TEST</HeadingSection>;
