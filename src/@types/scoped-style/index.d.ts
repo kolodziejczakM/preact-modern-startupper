f7fbf2438c14929c50bf3cbb827b03347f1f7344
kolodziejczakM <kolodziejczak.mn@gmail.com>
2019-10-17 22:00:58 +0200

    Updated preact-hooks-unistore (ts problem), wouter-preact(ts-jest problem), TS types for scoped-style WiP

--- src/@types/scoped-style/index.d.ts
+++ src/@types/scoped-style/index.d.ts
@@ -1,1 +1,28 @@
-declare module 'scoped-style';
+declare module 'scoped-style' {
+    type RuleInserter = (css: string) => void;
+
+    interface KeyframesCreator {
+        // TODO:
+    }
+    interface GlobalCreator {
+        // TODO:
+        (...args: any[]): any;
+    }
+
+    interface Styled {
+        (element: string): Function; // TODO: return type...
+        keyframes: KeyframesCreator; // TODO:
+        global: GlobalCreator; // TODO:
+    }
+
+    interface Scoped {
+        (createElement: Function, callback?: RuleInserter): Styled;
+        defaultCallback: RuleInserter;
+        generateID: () => string;
+    }
+
+    const styled: Styled;
+    const scoped: Scoped;
+
+    export default scoped;
+}
